96a302e8be2f9ff5d6dec75f2b99dc59cef85e44
vita-max <61422347+vita-max@users.noreply.github.com>
2022-08-11 15:48:18 +0300

    Image elements must have an alt prop

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -135,7 +135,7 @@ export default function SearchFilters() {
                     marginTop="5"
                     marginBottom="5"
                   >
-                    <Image src={noresult} />
+                    <Image src={noresult} alt="no results"/>
                     <Text fontSize="xl" marginTop="3">
                       Waiting to search!
                     </Text>
