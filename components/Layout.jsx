c97507a89c061220735d2c9152c14b297032c922
vita-max <61422347+vita-max@users.noreply.github.com>
2022-08-11 15:57:47 +0300

    check

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
-import Footer from './Footer';
-import Navbar from './Navbar';
+// import Footer from './Footer';
+// import Navbar from './Navbar';
 
 export default function Layout({ children }) {
   return (
@@ -11,11 +11,11 @@ export default function Layout({ children }) {
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
-          <Navbar />
+          Navbar 
         </header>
         <main>{children} </main>
         <footer>
-          <Footer/> 
+          Footer
         </footer>
       </Box>
     </>
